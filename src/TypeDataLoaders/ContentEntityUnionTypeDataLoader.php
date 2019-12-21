1dc0e6c94fd7499f412dbcc38a757cf5c315f86c
Leo <losoviz@gmail.com>
2019-12-21 18:04:26 +0800

    Implemented default UnionTypeDataLoader

--- src/TypeDataLoaders/ContentEntityUnionTypeDataLoader.php
+++ src/TypeDataLoaders/ContentEntityUnionTypeDataLoader.php
@@ -1,9 +1,13 @@
 <?php
 namespace PoP\Content\TypeDataLoaders;
 
-use PoP\ComponentModel\TypeDataLoaders\UnionTypeDataLoader;
+use PoP\ComponentModel\TypeDataLoaders\AbstractUnionTypeDataLoader;
+use PoP\Content\TypeResolvers\ContentEntityUnionTypeResolver;
 
-class ContentEntityUnionTypeDataLoader extends UnionTypeDataLoader
+class ContentEntityUnionTypeDataLoader extends AbstractUnionTypeDataLoader
 {
-
+    protected function getUnionTypeResolverClass(): string
+    {
+        return ContentEntityUnionTypeResolver::class;
+    }
 }
