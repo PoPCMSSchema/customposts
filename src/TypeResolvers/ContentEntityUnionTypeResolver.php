602dc35221cbe3e1f3d77366f7dd7f87ab9c01f6
Leo <losoviz@gmail.com>
2019-12-21 17:30:51 +0800

    Moved code under PostsWP

--- src/TypeResolvers/ContentEntityUnionTypeResolver.php
+++ src/TypeResolvers/ContentEntityUnionTypeResolver.php
@@ -2,7 +2,6 @@
 namespace PoP\Content\TypeResolvers;
 
 use PoP\Translation\Facades\TranslationAPIFacade;
-use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
 use PoP\ComponentModel\TypeResolvers\AbstractUnionTypeResolver;
 use PoP\Content\TypeDataLoaders\ContentEntityUnionTypeDataLoader;
 
@@ -24,30 +23,6 @@ class ContentEntityUnionTypeResolver extends AbstractUnionTypeResolver
     public function getTypeDataLoaderClass(): string
     {
         return ContentEntityUnionTypeDataLoader::class;
-    }
-
-    /**
-     * Overriding function to provide optimization:
-     * instead of calling ->isIDOfType on each object (as in parent function), in which case we must make a DB call for each result,
-     * we obtain all the types from executing a single query against the DB
-     *
-     * @param array $ids
-     * @return array
-     */
-    protected function getResultItemIDTargetTypeResolvers(array $ids): array
-    {
-        $resultItemIDTargetTypeResolvers = [];
-        $instanceManager = InstanceManagerFacade::getInstance();
-        $postUnionTypeDataLoader = $instanceManager->getInstance($this->getTypeDataLoaderClass());
-        if ($posts = $postUnionTypeDataLoader->getObjects($ids)) {
-            foreach ($posts as $post) {
-                $targetTypeResolver = $this->getTargetTypeResolver($post);
-                if (!is_null($targetTypeResolver)) {
-                    $resultItemIDTargetTypeResolvers[$targetTypeResolver->getID($post)] = $targetTypeResolver;
-                }
-            }
-        }
-        return $resultItemIDTargetTypeResolvers;
     }
 }
 
