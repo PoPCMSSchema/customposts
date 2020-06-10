d67e26073df2b0d7fb33cb3ed3da1f2dd64d26dd
Leonardo Losoviz <leo@getpop.org>
2020-06-10 16:49:05 +0800

    Initialize ComponentConfiguration

--- src/Component.php
+++ src/Component.php
@@ -29,6 +29,7 @@ class Component extends AbstractComponent
     protected static function doInitialize(array $configuration = [], bool $skipSchema = false): void
     {
         parent::doInitialize($configuration, $skipSchema);
+        ComponentConfiguration::setConfiguration($configuration);
         self::maybeInitYAMLSchemaServices(dirname(__DIR__), $skipSchema);
     }
 
