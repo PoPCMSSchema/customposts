3fc7706a7b84b5c916d5401e74ccf04f0779637a
Leonardo Losoviz <leo@getpop.org>
2020-06-17 16:04:45 +0800

    Added dependency to migrate package

--- src/Component.php
+++ src/Component.php
@@ -23,6 +23,13 @@ class Component extends AbstractComponent
         ];
     }
 
+    public static function getDependedMigrationPlugins(): array
+    {
+        return [
+            'migrate-customposts',
+        ];
+    }
+
     /**
      * Initialize services
      */
