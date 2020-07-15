a3fa2d494be0cb0eee027642fb059daa9adbd9f5
Leonardo Losoviz <leo@getpop.org>
2020-07-15 13:06:49 +0800

    Added field "customPost"

--- src/FieldResolvers/RootCustomPostListFieldResolver.php
+++ src/FieldResolvers/RootCustomPostListFieldResolver.php
@@ -4,12 +4,106 @@ declare(strict_types=1);
 
 namespace PoP\CustomPosts\FieldResolvers;
 
+use PoP\CustomPosts\Types\Status;
 use PoP\Engine\TypeResolvers\RootTypeResolver;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\CustomPosts\Facades\CustomPostTypeAPIFacade;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\CustomPosts\TypeHelpers\CustomPostUnionTypeHelpers;
+use PoP\CustomPosts\TypeResolvers\CustomPostUnionTypeResolver;
 
+/**
+ * Add the Custom Post fields to the Root
+ *
+ * @author Leonardo Losoviz <leo@getpop.org>
+ */
 class RootCustomPostListFieldResolver extends AbstractCustomPostListFieldResolver
 {
     public static function getClassesToAttachTo(): array
     {
         return array(RootTypeResolver::class);
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return array_merge(
+            parent::getFieldNamesToResolve(),
+            [
+                'customPost',
+            ]
+        );
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'customPost' => $translationAPI->__('Custom post with a specific ID', 'customposts'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+            'customPost' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
+        $translationAPI = TranslationAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'customPost':
+                return array_merge(
+                    $schemaFieldArgs,
+                    [
+                        [
+                            SchemaDefinition::ARGNAME_NAME => 'id',
+                            SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_ID,
+                            SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The custom post ID', 'customposts'),
+                            SchemaDefinition::ARGNAME_MANDATORY => true,
+                        ],
+                    ]
+                );
+        }
+        return $schemaFieldArgs;
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        $customPostTypeAPI = CustomPostTypeAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'customPost':
+                $query = [
+                    'types-from-union-resolver-class' => CustomPostUnionTypeResolver::class,
+                    'include' => [$fieldArgs['id']],
+                    'custom-post-status' => [
+                        Status::PUBLISHED,
+                    ],
+                ];
+                $options = [
+                    'return-type' => POP_RETURNTYPE_IDS,
+                ];
+                if ($posts = $customPostTypeAPI->getCustomPosts($query, $options)) {
+                    return $posts[0];
+                }
+                return null;
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
+    {
+        switch ($fieldName) {
+            case 'customPost':
+                return CustomPostUnionTypeHelpers::getCustomPostUnionOrTargetTypeResolverClass(CustomPostUnionTypeResolver::class);
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName, $fieldArgs);
     }
 }
