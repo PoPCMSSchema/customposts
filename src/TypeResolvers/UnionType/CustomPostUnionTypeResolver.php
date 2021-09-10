bb31877ae0d7122299d7bee8c6e0325214b64f65
leoloso <leo@getpop.org>
2021-09-10 11:58:52 +0000

    Merge pull request #1028 from leoloso/rename-Object-Union-Interface-folders-to-include-Type
    Rename `Object`, `Union` and `Interface` folders to include `Type`

--- src/TypeResolvers/UnionType/CustomPostUnionTypeResolver.php
+++ src/TypeResolvers/UnionType/CustomPostUnionTypeResolver.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace PoPSchema\CustomPosts\TypeResolvers\Union;
+namespace PoPSchema\CustomPosts\TypeResolvers\UnionType;
 
-use PoP\ComponentModel\TypeResolvers\Union\AbstractUnionTypeResolver;
-use PoPSchema\CustomPosts\RelationalTypeDataLoaders\Union\CustomPostUnionTypeDataLoader;
-use PoPSchema\CustomPosts\TypeResolvers\Interface\IsCustomPostInterfaceTypeResolver;
+use PoP\ComponentModel\TypeResolvers\UnionType\AbstractUnionTypeResolver;
+use PoPSchema\CustomPosts\RelationalTypeDataLoaders\UnionType\CustomPostUnionTypeDataLoader;
+use PoPSchema\CustomPosts\TypeResolvers\InterfaceType\IsCustomPostInterfaceTypeResolver;
 
 class CustomPostUnionTypeResolver extends AbstractUnionTypeResolver
 {
